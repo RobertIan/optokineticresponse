d473d518965169aa4ac9e59d0d1b8523e3e2b7f6
robert_ian <rianetheredge@gmail.com>
2015-04-17 16:37:11 -0500

    psychopy version 30 s interval with increasing angular scale factor and decreasing phase (should produce similar speeds)

--- optoMotorDisplay.py
+++ optoMotorDisplay.py
@@ -35,36 +35,39 @@ fullscreen = (1920, 1080)
 win2 = visual.Window(screen=0, size=fullscreen, pos=(0, 0))
 
 #create the stimuli
-opto1 = visual.GratingStim(win=win2, size=fullscreen, tex='sqr',  pos=[0,0], sf=100000)
-opto2 = visual.GratingStim(win=win2, size=fullscreen, tex='sqr', pos=[0,0], sf=10000)
-opto3 = visual.GratingStim(win=win2, size=fullscreen, tex='sqr', pos=[0,0], sf=1000)
+opto1 = visual.GratingStim(win=win2, size=fullscreen, tex='sqr',  pos=[0,0], sf=250000, color= [1,1,1])
+opto2 = visual.GratingStim(win=win2, size=fullscreen, tex='sqr', pos=[0,0], sf=25000, color= [1,1,1])
+opto3 = visual.GratingStim(win=win2, size=fullscreen, tex='sqr', pos=[0,0], sf=2500, color= [1,1,1])
 
 #draw the stimuli and update the window
 while 1:
     print clock.getTime()
     if interval1start<clock.getTime()<interval1stop : #this creates a never-ending loop
         opto1.setPhase(0.05, '+')#advance phase by 0.05 of a cycle
+        opto1.color -= [0.001, 0.001, 0.001]
         opto1.draw()
         win2.flip()
     if interval1stop<clock.getTime()<interval2start : #this creates a never-ending loop
         win2.flip()
     if interval2start<clock.getTime()<interval2stop : #this creates a never-ending loop
-        opto2.setPhase(0.005, '+')#advance phase by 0.05 of a cycle
+        opto2.setPhase(0.05, '+')#advance phase by 0.05 of a cycle
+        opto2.color -= [0.001, 0.001, 0.001]
         opto2.draw()
         win2.flip()
     if interval2stop<clock.getTime()<interval3start : #this creates a never-ending loop
         win2.flip()
     if interval3start<clock.getTime()<interval3stop : #this creates a never-ending loop
-        opto3.setPhase(0.0005, '+')#advance phase by 0.05 of a cycle
+        opto3.setPhase(0.05, '+')#advance phase by 0.05 of a cycle
+        opto3.color -= [0.001, 0.001, 0.001]
         opto3.draw()
         win2.flip()
     if interval3stop<clock.getTime()<triallength:
         win2.flip()
-        break
     if len(event.getKeys())>0:
         event.clearEvents()
         break
-
+    if triallength<clock.getTime():
+        break
 
 #cleanup
 win2.close()
